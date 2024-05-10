afa73fe7e6bb1bb3e0592a55f9e1fd56a5a9e859
kuriloalexey <aliaksei.kuryla@innowise-group.com>
2024-05-10 17:04:32 +0200

    JavPriceAggregator

---
+++ scripts/helpers/JavPriceAggregator/deploy.testnet.js
@@ -0,0 +1,32 @@
+const { ethers, upgrades } = require("hardhat");
+
+async function main() {
+    const [owner] = await ethers.getSigners();
+    // We get the contract to deploy
+    console.log(`Deploying from ${owner.address}`);
+    const Contract = await ethers.getContractFactory("JavPriceAggregator");
+    const contract = await upgrades.deployProxy(
+        Contract,
+        [
+            ["0x8D50eE492c9c3a8174aA39c377839944dc546378"], //_allowedAddresses_
+        ],
+        {
+            initializer: "initialize",
+            kind: "uups",
+            txOverrides: {
+                gasLimit: ethers.parseUnits("0.03", "gwei"),
+            },
+        },
+    );
+    await contract.waitForDeployment();
+
+    const contractAddress = await contract.getAddress();
+    console.log(`JavPriceAggregator contract deployed to: ${contractAddress}`);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
