b334a1aadbb2b49954c1088a5645efdaf2629097
kuriloalexey <aliaksei.kuryla@innowise-group.com>
2024-05-21 17:11:01 +0200

    JavTradingStorage

---
+++ contracts/trade/JavTradingStorage.sol
@@ -0,0 +1,245 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.23;
+
+import "../interfaces/trade/libraries/ITradingStorageUtils.sol";
+
+import "../libraries/trade/TradingStorageUtils.sol";
+import "../libraries/trade/ChainUtils.sol";
+import "./abstract/JavAddressStore.sol";
+
+/**
+ * @custom:version 8
+ * @dev Facet #5: Trading storage
+ */
+contract JavTradingStorage is JavAddressStore, ITradingStorageUtils {
+    // Initialization
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function initializeTradingStorage(
+        address _gns,
+        address _gnsStaking,
+        address[] memory _collaterals,
+        address[] memory _gTokens
+    ) external reinitializer(6) {
+        TradingStorageUtils.initializeTradingStorage(_gns, _gnsStaking, _collaterals, _gTokens);
+    }
+
+    // Management Setters
+
+    /// @inheritdoc ITradingStorageUtils
+    function updateTradingActivated(TradingActivated _activated) external onlyRole(Role.GOV) {
+        TradingStorageUtils.updateTradingActivated(_activated);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function addCollateral(address _collateral, address _gToken) external onlyRole(Role.GOV) {
+        TradingStorageUtils.addCollateral(_collateral, _gToken);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function toggleCollateralActiveState(uint8 _collateralIndex) external onlyRole(Role.GOV) {
+        TradingStorageUtils.toggleCollateralActiveState(_collateralIndex);
+    }
+
+    function updateGToken(address _collateral, address _gToken) external onlyRole(Role.GOV) {
+        TradingStorageUtils.updateGToken(_collateral, _gToken);
+    }
+
+    // Interactions
+
+    /// @inheritdoc ITradingStorageUtils
+    function storeTrade(
+        Trade memory _trade,
+        TradeInfo memory _tradeInfo
+    ) external virtual onlySelf returns (Trade memory) {
+        return TradingStorageUtils.storeTrade(_trade, _tradeInfo);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function updateTradeCollateralAmount(
+        ITradingStorage.Id memory _tradeId,
+        uint120 _collateralAmount
+    ) external virtual onlySelf {
+        TradingStorageUtils.updateTradeCollateralAmount(_tradeId, _collateralAmount);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function updateOpenOrderDetails(
+        ITradingStorage.Id memory _tradeId,
+        uint64 _openPrice,
+        uint64 _tp,
+        uint64 _sl,
+        uint16 _maxSlippageP
+    ) external virtual onlySelf {
+        TradingStorageUtils.updateOpenOrderDetails(_tradeId, _openPrice, _tp, _sl, _maxSlippageP);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function updateTradeTp(Id memory _tradeId, uint64 _newTp) external virtual onlySelf {
+        TradingStorageUtils.updateTradeTp(_tradeId, _newTp);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function updateTradeSl(Id memory _tradeId, uint64 _newSl) external virtual onlySelf {
+        TradingStorageUtils.updateTradeSl(_tradeId, _newSl);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function closeTrade(Id memory _tradeId) external virtual onlySelf {
+        TradingStorageUtils.closeTrade(_tradeId);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function storePendingOrder(
+        PendingOrder memory _pendingOrder
+    ) external virtual onlySelf returns (PendingOrder memory) {
+        return TradingStorageUtils.storePendingOrder(_pendingOrder);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function closePendingOrder(Id memory _orderId) external virtual onlySelf {
+        TradingStorageUtils.closePendingOrder(_orderId);
+    }
+
+    // Getters
+
+    /// @inheritdoc ITradingStorageUtils
+    function getCollateral(uint8 _index) external view returns (Collateral memory) {
+        return TradingStorageUtils.getCollateral(_index);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function isCollateralActive(uint8 _index) external view returns (bool) {
+        return TradingStorageUtils.isCollateralActive(_index);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function isCollateralListed(uint8 _index) external view returns (bool) {
+        return TradingStorageUtils.isCollateralListed(_index);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getCollateralsCount() external view returns (uint8) {
+        return TradingStorageUtils.getCollateralsCount();
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getCollaterals() external view returns (Collateral[] memory) {
+        return TradingStorageUtils.getCollaterals();
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getCollateralIndex(address _collateral) external view returns (uint8) {
+        return TradingStorageUtils.getCollateralIndex(_collateral);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getTradingActivated() external view returns (TradingActivated) {
+        return TradingStorageUtils.getTradingActivated();
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getTraderStored(address _trader) external view returns (bool) {
+        return TradingStorageUtils.getTraderStored(_trader);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getTraders(uint32 _offset, uint32 _limit) external view returns (address[] memory) {
+        return TradingStorageUtils.getTraders(_offset, _limit);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getTrade(address _trader, uint32 _index) external view returns (Trade memory) {
+        return TradingStorageUtils.getTrade(_trader, _index);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getTrades(address _trader) external view returns (Trade[] memory) {
+        return TradingStorageUtils.getTrades(_trader);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getAllTrades(uint256 _offset, uint256 _limit) external view returns (Trade[] memory) {
+        return TradingStorageUtils.getAllTrades(_offset, _limit);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getTradeInfo(address _trader, uint32 _index) external view returns (TradeInfo memory) {
+        return TradingStorageUtils.getTradeInfo(_trader, _index);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getTradeInfos(address _trader) external view returns (TradeInfo[] memory) {
+        return TradingStorageUtils.getTradeInfos(_trader);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getAllTradeInfos(
+        uint256 _offset,
+        uint256 _limit
+    ) external view returns (TradeInfo[] memory) {
+        return TradingStorageUtils.getAllTradeInfos(_offset, _limit);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getPendingOrder(Id memory _orderId) external view returns (PendingOrder memory) {
+        return TradingStorageUtils.getPendingOrder(_orderId);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getPendingOrders(address _user) external view returns (PendingOrder[] memory) {
+        return TradingStorageUtils.getPendingOrders(_user);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getAllPendingOrders(
+        uint256 _offset,
+        uint256 _limit
+    ) external view returns (PendingOrder[] memory) {
+        return TradingStorageUtils.getAllPendingOrders(_offset, _limit);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getTradePendingOrderBlock(
+        Id memory _tradeId,
+        PendingOrderType _orderType
+    ) external view returns (uint256) {
+        return TradingStorageUtils.getTradePendingOrderBlock(_tradeId, _orderType);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getCounters(
+        address _trader,
+        CounterType _type
+    ) external view returns (Counter memory) {
+        return TradingStorageUtils.getCounters(_trader, _type);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getPendingOpenOrderType(
+        TradeType _tradeType
+    ) external pure returns (PendingOrderType) {
+        return TradingStorageUtils.getPendingOpenOrderType(_tradeType);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getGToken(uint8 _collateralIndex) external view returns (address) {
+        return TradingStorageUtils.getGToken(_collateralIndex);
+    }
+
+    /// @inheritdoc ITradingStorageUtils
+    function getPnlPercent(
+        uint64 _openPrice,
+        uint64 _currentPrice,
+        bool _long,
+        uint24 _leverage
+    ) external pure returns (int256) {
+        return TradingStorageUtils.getPnlPercent(_openPrice, _currentPrice, _long, _leverage);
+    }
+}
