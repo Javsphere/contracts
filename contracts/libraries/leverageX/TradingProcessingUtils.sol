0295530bf1b3c5743518b1c7df1bcc799ec2f241
kuriloalexey <aliaksei.kuryla@innowise-group.com>
2024-11-13 12:31:24 +0100

    fix TradingProcessingUtils

--- contracts/libraries/leverageX/TradingProcessingUtils.sol
+++ contracts/libraries/leverageX/TradingProcessingUtils.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.23;
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "../../interfaces/leverageX/IJavMultiCollatDiamond.sol";
 import "../../interfaces/leverageX/IJavBorrowingProvider.sol";
-import "../../interfaces/IRewardsDistributor.sol";
 
 import "./StorageUtils.sol";
 import "./AddressStoreUtils.sol";
