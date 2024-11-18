38c8ec5506f1b68f6ef98ebb735ee2637c3b0295
kuriloalexey <aliaksei.kuryla@innowise-group.com>
2024-11-18 10:23:02 +0100

    update burnTokens events

--- contracts/interfaces/IJavFreezer.sol
+++ contracts/interfaces/IJavFreezer.sol
@@ -102,7 +102,13 @@ interface IJavFreezer is IGeneralErrors {
     event SetInfinityPassPercent(uint256 indexed _percent);
     event SetInfinityPass(address indexed _address);
     event SetMigratorAddress(address indexed _address);
-    event BurnTokens(uint256 indexed pid, address indexed holder, uint256 lockId, uint256 amount);
+    event BurnTokens(
+        uint256 indexed pid,
+        address indexed holder,
+        uint256 depositId,
+        uint256 lockId,
+        uint256 amount
+    );
     event SetAverageBlockTime(uint256 indexed blockTime);
 
     error WrongPool();
