0100b5559409da082eb87dc2648be69c7abe93d0
kuriloalexey <aliaksei.kuryla@innowise-group.com>
2024-12-10 17:27:57 +0100

    fixes

--- contracts/helpers/LPProvider.sol
+++ contracts/helpers/LPProvider.sol
@@ -1,32 +1,29 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.16;
+pragma solidity ^0.8.26;
 
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "../interfaces/INonfungiblePositionManager.sol";
 import "../interfaces/IRewardsDistributor.sol";
-import "../interfaces/IVanillaRouter02.sol";
 import "../interfaces/IERC20Extended.sol";
-import "../interfaces/IWDFI.sol";
+import "../interfaces/IPoolFactory.sol";
 import "../base/BaseUpgradable.sol";
+import "../interfaces/IRouter.sol";
+import "../interfaces/IPool.sol";
 
-contract LPProvider is IERC721Receiver, BaseUpgradable {
+contract LPProvider is BaseUpgradable {
     using SafeERC20 for IERC20;
 
-    INonfungiblePositionManager public nonfungiblePositionManager;
-    ISwapRouter public swapRouter;
-    IVanillaRouter02 public routerV2;
+    IPoolFactory public poolFactory;
+    IRouter public router;
 
     address public botAddress;
     uint256 public burnPercent;
     address public stakingAddress;
 
-    mapping(address => uint256) public lpLockAmountV2;
-    mapping(uint256 => uint256) public lpLockAmountV3;
-    uint256[] public pairsTokenId;
-    address public wdfiAddress;
+    mapping(address => uint256) public lpLockAmount;
+    address[] public pools;
     address public rewardsDistributorAddress;
 
     /* ========== EVENTS ========== */
@@ -34,18 +31,9 @@ contract LPProvider is IERC721Receiver, BaseUpgradable {
     event SetRewardsDistributorAddress(address indexed _address);
     event AddLiquidity(uint256 amountA, uint256 amountB, uint256 liquidity);
     event AddLiquidityETH(uint256 amountToken, uint256 amountETH, uint256 liquidity);
-    event SetWDFIAddress(address indexed _address);
-    event SwapToWDFI(uint256 indexed amount);
-    event SwapToDFI(uint256 indexed amount);
-    event WithdrawDFI(address indexed to, uint256 amount);
+    event WithdrawNative(address indexed to, uint256 amount);
     event Withdraw(address indexed token, address indexed to, uint256 amount);
-    event RemoveLiquidityV2(address indexed token, address indexed to, uint256 amount);
-    event RemoveLiquidityV3(
-        address indexed token,
-        uint256 nftId,
-        address indexed to,
-        uint256 amount
-    );
+    event PoolCreated(address pool);
 
     modifier onlyBot() {
         require(msg.sender == botAddress, "LPProvider: only bot");
@@ -62,14 +50,12 @@ contract LPProvider is IERC721Receiver, BaseUpgradable {
     fallback() external payable {}
 
     function initialize(
-        address _nonfungiblePositionManager,
-        address _routerAddressV2,
-        address _swapRouter,
+        address _poolFactory,
+        address _routerAddress,
         address _botAddress
     ) external initializer {
-        nonfungiblePositionManager = INonfungiblePositionManager(_nonfungiblePositionManager);
-        swapRouter = ISwapRouter(_swapRouter);
-        routerV2 = IVanillaRouter02(_routerAddressV2);
+        poolFactory = IPoolFactory(_poolFactory);
+        router = IRouter(_routerAddress);
 
         botAddress = _botAddress;
 
@@ -90,39 +76,6 @@ contract LPProvider is IERC721Receiver, BaseUpgradable {
         emit SetRewardsDistributorAddress(_address);
     }
 
-    function setWDFIAddress(address _address) external onlyAdmin {
-        wdfiAddress = _address;
-        emit SetWDFIAddress(_address);
-    }
-
-    function onERC721Received(
-        address operator,
-        address from,
-        uint256 tokenId,
-        bytes calldata
-    ) external returns (bytes4) {
-        return IERC721Receiver.onERC721Received.selector;
-    }
-
-    function swapToWDFI(uint256 _amount) external onlyAdmin {
-        require(address(this).balance >= _amount, "LPProvider: Invalid balance - dfi");
-
-        IWDFI(wdfiAddress).deposit{value: _amount}();
-
-        emit SwapToWDFI(_amount);
-    }
-
-    function swapToDFI(uint256 _amount) external onlyAdmin {
-        require(
-            IWDFI(wdfiAddress).balanceOf(address(this)) >= _amount,
-            "LPProvider: Invalid balance - dfi"
-        );
-
-        IWDFI(wdfiAddress).withdraw(_amount);
-
-        emit SwapToDFI(_amount);
-    }
-
     /**
      * @notice Functon to withdraw amount
      * @param _token token address
@@ -136,125 +89,26 @@ contract LPProvider is IERC721Receiver, BaseUpgradable {
         emit Withdraw(_token, _to, _amount);
     }
 
-    function withdrawDFI(address payable _to, uint256 _amount) external onlyAdmin {
+    function withdrawNative(address payable _to, uint256 _amount) external onlyAdmin {
         require(address(this).balance >= _amount, "LPProvider: Invalid amount");
 
         _to.transfer(_amount);
 
-        emit WithdrawDFI(_to, _amount);
+        emit WithdrawNative(_to, _amount);
     }
 
-    function removeLiquidityV2(
-        address _pairAddress,
-        address _tokenA,
-        address _tokenB,
-        address _to
-    ) external onlyAdmin {
-        uint256 liquidity = IERC20(_pairAddress).balanceOf(address(this));
+    function createPool(address tokenA, address tokenB, uint24 fee) external onlyAdmin {
+        address _poolAddress = poolFactory.createPool(tokenA, tokenB, fee);
+        pools.push(_poolAddress);
 
-        IERC20(_pairAddress).safeDecreaseAllowance(address(routerV2), 0);
-        IERC20(_pairAddress).safeIncreaseAllowance(address(routerV2), liquidity);
-
-        (uint amountA, uint amountB) = routerV2.removeLiquidity(
-            _tokenA,
-            _tokenB,
-            liquidity,
-            1,
-            1,
-            _to,
-            block.timestamp + 1000
-        );
-
-        emit RemoveLiquidityV2(_tokenA, _to, amountA);
-        emit RemoveLiquidityV2(_tokenB, _to, amountB);
+        emit PoolCreated(_poolAddress);
     }
 
-    function removeLiquidityV3(uint256 _tokenId, address _to) external onlyAdmin {
-        (
-            ,
-            ,
-            address token0,
-            address token1,
-            ,
-            ,
-            ,
-            uint128 liquidity,
-            ,
-            ,
-            ,
-
-        ) = nonfungiblePositionManager.positions(_tokenId);
-
-        INonfungiblePositionManager.DecreaseLiquidityParams
-            memory params = INonfungiblePositionManager.DecreaseLiquidityParams({
-                tokenId: _tokenId,
-                liquidity: liquidity,
-                amount0Min: 0,
-                amount1Min: 0,
-                deadline: block.timestamp + 1000
-            });
-
-        (uint256 amount0, uint256 amount1) = nonfungiblePositionManager.decreaseLiquidity(params);
-
-        INonfungiblePositionManager.CollectParams memory feeParams = INonfungiblePositionManager
-            .CollectParams({
-                tokenId: _tokenId,
-                recipient: _to,
-                amount0Max: type(uint128).max,
-                amount1Max: type(uint128).max
-            });
-
-        nonfungiblePositionManager.collect(feeParams);
-        nonfungiblePositionManager.burn(_tokenId);
-
-        emit RemoveLiquidityV3(token0, _tokenId, _to, amount0);
-        emit RemoveLiquidityV3(token1, _tokenId, _to, amount1);
-    }
-
-    function addLiquidityV3(
-        uint256 tokenId,
-        address token0,
-        address token1,
-        uint256 amount0Desired,
-        uint256 amount1Desired
-    ) external onlyAdmin {
-        require(
-            IERC20(token0).balanceOf(address(this)) >= amount0Desired,
-            "LPProvider: Invalid balance - token0"
-        );
-        require(
-            IERC20(token1).balanceOf(address(this)) >= amount1Desired,
-            "LPProvider: Invalid balance - token1"
-        );
-
-        IERC20(token0).safeDecreaseAllowance(address(nonfungiblePositionManager), 0);
-        IERC20(token1).safeDecreaseAllowance(address(nonfungiblePositionManager), 0);
-
-        IERC20(token0).safeIncreaseAllowance(address(nonfungiblePositionManager), amount0Desired);
-        IERC20(token1).safeIncreaseAllowance(address(nonfungiblePositionManager), amount1Desired);
-
-        INonfungiblePositionManager.IncreaseLiquidityParams
-            memory params = INonfungiblePositionManager.IncreaseLiquidityParams({
-                tokenId: tokenId,
-                amount0Desired: amount0Desired,
-                amount1Desired: amount1Desired,
-                amount0Min: amount0Desired,
-                amount1Min: amount1Desired,
-                deadline: block.timestamp + 10000
-            });
-
-        (uint128 liquidity, uint256 amount0, uint256 amount1) = nonfungiblePositionManager
-            .increaseLiquidity(params);
-
-        lpLockAmountV3[tokenId] += liquidity;
-
-        emit AddLiquidity(amount0, amount1, liquidity);
-    }
-
-    function addLiquidityV2(
+    function addLiquidity(
         address lpToken,
         address tokenA,
         address tokenB,
+        bool stable,
         uint256 amountADesired,
         uint256 amountBDesired
     ) external onlyAdmin {
@@ -267,15 +121,16 @@ contract LPProvider is IERC721Receiver, BaseUpgradable {
             "LPProvider: Invalid balance - tokenB"
         );
 
-        IERC20(tokenA).safeDecreaseAllowance(address(routerV2), 0);
-        IERC20(tokenB).safeDecreaseAllowance(address(routerV2), 0);
+        IERC20(tokenA).safeDecreaseAllowance(address(router), 0);
+        IERC20(tokenB).safeDecreaseAllowance(address(router), 0);
 
-        IERC20(tokenA).safeIncreaseAllowance(address(routerV2), amountADesired);
-        IERC20(tokenB).safeIncreaseAllowance(address(routerV2), amountBDesired);
+        IERC20(tokenA).safeIncreaseAllowance(address(router), amountADesired);
+        IERC20(tokenB).safeIncreaseAllowance(address(router), amountBDesired);
 
-        (uint256 amountA, uint256 amountB, uint256 liquidity) = routerV2.addLiquidity(
+        (uint256 amountA, uint256 amountB, uint256 liquidity) = router.addLiquidity(
             tokenA,
             tokenB,
+            stable,
             amountADesired,
             amountBDesired,
             0,
@@ -284,43 +139,25 @@ contract LPProvider is IERC721Receiver, BaseUpgradable {
             block.timestamp + 1000
         );
 
-        lpLockAmountV2[lpToken] += liquidity;
+        lpLockAmount[lpToken] += liquidity;
 
         emit AddLiquidity(amountA, amountB, liquidity);
     }
 
-    function addLiquidityETHV2(
-        address lpToken,
-        address token,
-        uint256 amountETH,
-        uint256 amountTokenDesired
-    ) external onlyAdmin {
-        require(address(this).balance >= amountETH, "LPProvider: Invalid balance - amountETH");
-        require(
-            IERC20(token).balanceOf(address(this)) >= amountTokenDesired,
-            "LPProvider: Invalid balance - amountTokenDesired"
-        );
-
-        IERC20(token).safeDecreaseAllowance(address(routerV2), 0);
-        IERC20(token).safeIncreaseAllowance(address(routerV2), amountTokenDesired);
-
-        (uint256 amountToken, uint256 amountETH_, uint256 liquidity) = routerV2.addLiquidityETH{
-            value: amountETH
-        }(token, amountTokenDesired, 0, 0, address(this), block.timestamp + 1000);
-
-        lpLockAmountV2[lpToken] += liquidity;
-
-        emit AddLiquidityETH(amountToken, amountETH_, liquidity);
-    }
-
-    function claimAndDistributeRewards(uint256[] memory _tokenIds) external onlyBot {
-        address[] memory _tokens = new address[](_tokenIds.length * 2);
-        for (uint256 i = 0; i < _tokenIds.length; ++i) {
-            (, , address token0, address token1, , , , , , , , ) = nonfungiblePositionManager
-                .positions(_tokenIds[i]);
-            _collectFees(_tokenIds[i]);
-            _tokens = _insertToken(_tokens, token0);
-            _tokens = _insertToken(_tokens, token1);
+    function claimAndDistributeRewards(address[] calldata _pools) external onlyBot {
+        address[] memory _tokens = new address[](_pools.length * 2);
+        for (uint256 i = 0; i < _pools.length; ++i) {
+            IPool _pool = IPool(_pools[i]);
+            (address token0, address token1) = _pool.tokens();
+            (uint256 claimed0, uint256 claimed1) = _pool.claimFees();
+            if (claimed0 > 0) {
+                _tokens = _insertToken(_tokens, token0);
+                IERC20(token0).safeTransfer(rewardsDistributorAddress, claimed0);
+            }
+            if (claimed1 > 0) {
+                _tokens = _insertToken(_tokens, token1);
+                IERC20(token1).safeTransfer(rewardsDistributorAddress, claimed1);
+            }
         }
         IRewardsDistributor(rewardsDistributorAddress).distributeRewards(_tokens);
     }
@@ -341,17 +178,5 @@ contract LPProvider is IERC721Receiver, BaseUpgradable {
             }
         }
         return _tokens;
-    }
-
-    function _collectFees(uint256 tokenId) private {
-        INonfungiblePositionManager.CollectParams memory params = INonfungiblePositionManager
-            .CollectParams({
-                tokenId: tokenId,
-                recipient: rewardsDistributorAddress,
-                amount0Max: type(uint128).max,
-                amount1Max: type(uint128).max
-            });
-
-        nonfungiblePositionManager.collect(params);
     }
 }
