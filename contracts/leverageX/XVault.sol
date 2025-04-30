0ff5f90abf6cd5160f5d0dcba20dc95813506e23
kuriloalexey <aliaksei.kuryla@innowise-group.com>
2025-04-30 14:55:28 +0200

    add pnlHandler check

--- contracts/leverageX/XVault.sol
+++ contracts/leverageX/XVault.sol
@@ -221,6 +221,7 @@ contract XVault is ERC4626Upgradeable, BaseUpgradable, IXVault, TermsAndCondUtil
     // @param __: a placeholder to match the function selector
     function distributeReward(uint8 __, uint256 assets) external {
         address sender = _msgSender();
+        if (sender != pnlHandler) revert OnlyTradingPnlHandler();
         _assetIERC20().safeTransferFrom(sender, address(this), assets);
 
         accRewardsPerToken +=
